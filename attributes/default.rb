126f40901969c550ae567ef672b8477e281946d8
Fred Thompson <fred.thompson@buildempire.co.uk>
2014-06-18 11:25:28 +0100

    Updated.

--- attributes/default.rb
+++ attributes/default.rb
@@ -20,6 +20,6 @@ default['cookbook_moodle']['oracle']['basic_rpm'] = 'oracle-instantclient12.1-ba
 default['cookbook_moodle']['oracle']['basic_zip'] = 'instantclient-basic-linux.x64-12.1.0.1.0.zip'
 default['cookbook_moodle']['oracle']['sdk_zip'] = 'instantclient-sdk-linux.x64-12.1.0.1.0.zip'
 default['cookbook_moodle']['oracle']['folder_name'] = 'instantclient_12_1'
-default['cookbook_moodle']['oracle']['version'] = default['cookbook_moodle']['basic_rpm'].split('-')[1].sub(/instantclient/, '')
+default['cookbook_moodle']['oracle']['version'] = default['cookbook_moodle']['oracle']['basic_rpm'].split('-')[1].sub(/instantclient/, '')
 
 default[:build_essential][:compile_time] = true
