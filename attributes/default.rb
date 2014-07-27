612a39c364c2c31638b9724570d42d9c83d99e5a
Fred Thompson <fred.thompson@buildempire.co.uk>
2014-07-27 20:07:56 +0100

    Cracked the cron job, at last!

--- attributes/default.rb
+++ attributes/default.rb
@@ -14,7 +14,7 @@ default['cookbook_moodle']['download'] = true
 default['cookbook_moodle']['branch'] = 'MOODLE_24_STABLE'
 default['cookbook_moodle']['cronic'] = '/opt/bin/cronic'
 default['cookbook_moodle']['cron_hour'] = "*"
-default['cookbook_moodle']['cron_minute'] = "5"
+default['cookbook_moodle']['cron_minute'] = "*/5"
 
 default['cookbook_moodle']['oracle']['basic_rpm'] = 'oracle-instantclient12.1-basic-12.1.0.1.0-1.x86_64.rpm'
 default['cookbook_moodle']['oracle']['basic_zip'] = 'instantclient-basic-linux.x64-12.1.0.1.0.zip'
