def216157238054083070cc4d39bf8a650c56d10
Fred Thompson <fred.thompson@buildempire.co.uk>
2014-08-14 15:10:33 +0100

    Forgot to include newrelic.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'fred.thompson@buildempire.co.uk'
 license          'Apache 2.0'
 description      'The Moodle cookbook, a fresh moodle deployment'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.5.4'
+version          '0.5.5'
 
 recipe 'cookbook_moodle', 'The Moodle cookbook, a fresh moodle deployment.'
 
@@ -12,6 +12,6 @@ recipe 'cookbook_moodle', 'The Moodle cookbook, a fresh moodle deployment.'
   supports os
 end
 
-%w{apt build-essential appbox database git magic_shell nginx php php-fpm htpasswd xml}.each do |cb|
+%w{apt build-essential appbox database git magic_shell newrelic nginx php php-fpm htpasswd xml}.each do |cb|
   depends cb
 end
