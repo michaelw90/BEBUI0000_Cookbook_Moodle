e07f1e024ffdff3e634d65bf834ed84e95a3aab9
Fred Thompson <fred.thompson@buildempire.co.uk>
2014-08-15 17:55:26 +0100

    Making an attempt to fix newrelic.

--- recipes/newrelic.rb
+++ recipes/newrelic.rb
@@ -7,6 +7,9 @@ if node['cookbook_moodle']['newrelic']
   newrelic_licence_key = node['cookbook_moodle']['newrelic']['license_key']
   if newrelic_licence_key
     node.set['newrelic']['license'] = newrelic_licence_key
+    node.set['newrelic']['server_monitoring'] = newrelic_licence_key
+    node.set['newrelic']['application_monitoring'] = newrelic_licence_key
+    node.set['newrelic']['plugin_monitoring'] = newrelic_licence_key
     node.set['newrelic']['install_dir'] = '/opt/newrelic'
     node.set['newrelic']['php-agent']['web_server']['service_name'] = 'nginx'
     include_recipe 'newrelic::repository'
