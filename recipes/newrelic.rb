75c538bd06b01f182b65192a77d8d8b0a0bddbb2
Fred Thompson <fred.thompson@buildempire.co.uk>
2014-08-15 18:05:01 +0100

    Debugging to the new relic code.

--- recipes/newrelic.rb
+++ recipes/newrelic.rb
@@ -6,10 +6,20 @@
 if node['cookbook_moodle']['newrelic']
   newrelic_licence_key = node['cookbook_moodle']['newrelic']['license_key']
   if newrelic_licence_key
+
+    Chef::Log.info %(Set newrelic license to node['cookbook_moodle']['newrelic']['license_key'])
+    Chef::Log.info newrelic_licence_key
+
     node.set['newrelic']['license'] = newrelic_licence_key
     node.set['newrelic']['server_monitoring'] = newrelic_licence_key
     node.set['newrelic']['application_monitoring'] = newrelic_licence_key
     node.set['newrelic']['plugin_monitoring'] = newrelic_licence_key
+
+    node.set['newrelic']['license'] = nil
+    node.set['newrelic']['server_monitoring']['license'] = node['newrelic']['license']
+    node.set['newrelic']['application_monitoring']['license'] = node['newrelic']['license']
+    node.set['newrelic']['plugin_monitoring']['license'] = node['newrelic']['license']
+
     node.set['newrelic']['install_dir'] = '/opt/newrelic'
     node.set['newrelic']['php-agent']['web_server']['service_name'] = 'nginx'
     include_recipe 'newrelic::repository'
