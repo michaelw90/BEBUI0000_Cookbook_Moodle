59684896868519331e2efdccea8e160043b4cebe
Fred Thompson <fred.thompson@buildempire.co.uk>
2014-12-16 18:20:45 +0000

    Changing to use the new mysql cookbook version, which has changed behaviour.

--- recipes/mysql.rb
+++ recipes/mysql.rb
@@ -7,42 +7,47 @@ root_password = node['cookbook_moodle']['database']['root_password']
 if root_password
   Chef::Log.info %(Set root password to node['cookbook_moodle']['database']['root_password'])
   Chef::Log.info root_password
-  node.set['mysql']['server_root_password'] = root_password
-  node.set['mysql']['server_repl_password'] = root_password
-  node.set['mysql']['server_debian_password'] = root_password
-end
 
-# Include the mysql recipes
-include_recipe "mysql::server"
-include_recipe "mysql::client"
-include_recipe "database::mysql"
+  # Include the mysql recipes
+  mysql_service 'default' do
+    initial_root_password root_password
+    action [:create, :start]
+  end
 
-# Setup the connection information
-mysql_connection_info = {
-    :host => 'localhost',
-    :username => 'root',
-    :password => root_password
-}
+  mysql_client 'default' do
+    action :create
+  end
 
-mysql_database node['cookbook_moodle']['database']['database_name'] do
-  connection mysql_connection_info
-  encoding 'UTF8'
-  collation 'utf8_unicode_ci'
-  action :create
-end
+  include_recipe "database::mysql"
 
-mysql_database_user node['cookbook_moodle']['database']['username'] do
-  connection mysql_connection_info
-  action [:create, :grant]
-  password(node['cookbook_moodle']['database']['password'])
-end
+  # Setup the connection information
+  mysql_connection_info = {
+      :host => 'localhost',
+      :username => 'root',
+      :password => root_password
+  }
 
-template '/etc/mysql/conf.d/mysite.cnf' do
-  owner 'mysql'
-  owner 'mysql'
-  source 'mysite.cnf.erb'
-  variables(
-      :max_allowed_packet => node['cookbook_moodle']['mysql']['max_allowed_packet']
-  )
-  notifies :restart, 'mysql_service[default]'
+  mysql_database node['cookbook_moodle']['database']['database_name'] do
+    connection mysql_connection_info
+    encoding 'UTF8'
+    collation 'utf8_unicode_ci'
+    action :create
+  end
+
+  mysql_database_user node['cookbook_moodle']['database']['username'] do
+    connection mysql_connection_info
+    action [:create, :grant]
+    password(node['cookbook_moodle']['database']['password'])
+  end
+
+  template '/etc/mysql/conf.d/mysite.cnf' do
+    owner 'mysql'
+    owner 'mysql'
+    source 'mysite.cnf.erb'
+    variables(
+        :max_allowed_packet => node['cookbook_moodle']['mysql']['max_allowed_packet']
+    )
+    notifies :restart, 'mysql_service[default]'
+  end
+
 end
