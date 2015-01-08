a3fd52e72384446ab56d753a876cee53aa7c3c47
Fred Thompson <fred.thompson@buildempire.co.uk>
2015-01-08 11:11:22 +0000

    Trying to re-enable the newer mysql cookbook again style again, to see if other cookbooks have caught up yet.

--- recipes/mysql.rb
+++ recipes/mysql.rb
@@ -13,20 +13,20 @@ if root_password
 end
 
 # Include the mysql recipes
-include_recipe "mysql::server"
-include_recipe "mysql::client"
+#include_recipe "mysql::server"
+#include_recipe "mysql::client"
 include_recipe "database::mysql"
 
 
 # Include the mysql recipes
-##mysql_service 'default' do
-##  initial_root_password root_password
-##  action [:create, :start]
-##end
+mysql_service 'default' do
+  initial_root_password root_password
+  action [:create, :start]
+end
 
-##mysql_client 'default' do
-##  action :create
-##end
+mysql_client 'default' do
+  action :create
+end
 
 
 # Setup the connection information
