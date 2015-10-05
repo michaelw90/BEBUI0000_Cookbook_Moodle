99a4af3adfb7b4a72618ee5363a565defc473e02
Michael Wright <michael@wserver.co.uk>
2015-10-05 19:28:20 +0100

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -51,3 +51,4 @@ default['cookbook_moodle']['rackspace']['backup_container'] = ''
 default['build-essential']['compile_time'] = true
 
 default['cookbook_moodle']['aws']['efs'] = ''
+default['cookbook_moodle']['aws']['elasticache'] = ''
