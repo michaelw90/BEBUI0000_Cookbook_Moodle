61af069615a94a3fdf777c6d97221fba847bf0c5
Michael Wright <michael.wright@buildempire.co.uk>
2015-10-05 13:18:17 +0100

    Tweak command

--- recipes/aws.rb
+++ recipes/aws.rb
@@ -9,7 +9,7 @@ efs = node['cookbook_moodle']['aws']['efs']
 if efs != ''
 
   execute "Attach EFS Share" do
-  	command "mkdir /efs && sudo mount -t nfs4 $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).#{efs}.efs.us-west-2.amazonaws.com:/ /efs"
+  	command "sudo mount -t nfs4 $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).#{efs}.efs.us-west-2.amazonaws.com:/ /efs"
   end
 
 end
