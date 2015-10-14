5780c240bf8b2e6a99322a3b334cff709d3e638d
Michael Wright <michael.wright@buildempire.co.uk>
2015-10-14 11:19:11 +0100

    Attach EFS

--- recipes/aws.rb
+++ recipes/aws.rb
@@ -9,7 +9,7 @@ efs = node['cookbook_moodle']['aws']['efs']
 if efs != ''
 
   execute "Attach EFS Share" do
-  	command "sudo mount -t nfs4 $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).#{efs}.efs.us-west-2.amazonaws.com:/ /efs"
+  	command "mkdir -p /efs && sudo mount -t nfs4 $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).#{efs}.efs.us-west-2.amazonaws.com:/ /efs"
   end
 
 end
