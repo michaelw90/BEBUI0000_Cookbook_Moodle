871130bb2a087bd5150623e27a2fcac588e26d8b
Michael Wright <michael.wright@buildempire.co.uk>
2016-03-14 15:01:37 +0000

    #BEAPO0001-1978 Fixed

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -22,6 +22,25 @@ node[:deploy].each do |application, deploy|
     app application
   end
 
+  # write out opsworks.php
+  template "#{deploy[:deploy_to]}/shared/config/opsworks.php" do
+    cookbook 'php'
+    source 'opsworks.php.erb'
+    mode '0660'
+    owner deploy[:user]
+    group deploy[:group]
+    variables(
+      :database => deploy[:database],
+      :memcached => deploy[:memcached],
+      :layers => node[:opsworks][:layers],
+      :stack_name => node[:opsworks][:stack][:name],
+      :environment_variables => deploy[:environment_variables]
+    )
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}/shared/config")
+    end
+  end
+
   execute "build LESS" do
     cwd "#{deploy[:current_path]}/public/theme/innovators"
     command "npm install && npm install -g grunt-cli && grunt less"
