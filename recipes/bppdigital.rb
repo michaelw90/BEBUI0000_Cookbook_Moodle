b06bdd58fdcaf6446895cc0f42b5ebe930d9f1b4
Michael Wright <michael.wright@buildempire.co.uk>
2016-05-13 11:04:00 +0100

    Add BPPDigital

---
+++ recipes/bppdigital.rb
@@ -0,0 +1,44 @@
+#
+# Cookbook Name:: cookbook_moodle
+# Recipe:: deploy
+#
+
+include_recipe 'deploy'
+
+node[:deploy].each do |application, deploy|
+  if deploy[:application_type] != 'php'
+    Chef::Log.debug("Skipping deploy::php application #{application} as it is not an PHP app")
+    next
+  end
+
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
+
+  opsworks_deploy do
+    deploy_data deploy
+    app application
+  end
+
+  # write out opsworks.php
+  template "#{deploy[:deploy_to]}/shared/config/opsworks.php" do
+    cookbook 'cookbook_moodle'
+    source 'opsworks.php.erb'
+    mode '0660'
+    owner deploy[:user]
+    group deploy[:group]
+    variables(
+      :database => deploy[:database],
+      :memcached => deploy[:memcached],
+      :layers => node[:opsworks][:layers],
+      :stack_name => node[:opsworks][:stack][:name],
+      :environment_variables => deploy[:environment_variables]
+    )
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}/shared/config")
+    end
+  end
+
+end
